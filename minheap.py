8d58a1036e7030c397bd4c7e043a8d12c8704650
Sezgin Seven <sezgin_seven@windowslive.com>
2024-09-08 00:31:36 +0300

    Update minheap.py

--- minheap.py
+++ minheap.py
@@ -1,71 +1,87 @@
 class MinHeap:
-    def __init__(self,max_size) :
+    def __init__(self, max_size):
         self.max_size = max_size
         self.size = 0
         self.Heap = [0] * (self.max_size + 1)
-
     
-    def getLeft(self,pos):
-        return 2*pos
+    def getLeft(self, pos):
+        return 2 * pos
 
-    def getRight(self,pos):
-        return 2*pos + 1
+    def getRight(self, pos):
+        return 2 * pos + 1
     
-    def getParent(self,pos):
+    def getParent(self, pos):
         return pos // 2
 
-    def isLeaf(self,pos):
-        return pos*2 > self.size
+    def isLeaf(self, pos):
+        return pos * 2 > self.size
 
-    def swap(self,fpos,spos):
-        self.Heap[fpos],self.Heap[spos] = self.Heap[spos],self.Heap[fpos]
+    def swap(self, fpos, spos):
+        self.Heap[fpos], self.Heap[spos] = self.Heap[spos], self.Heap[fpos]
 
-    def minHeapify(self,pos):
+    def minHeapify(self, pos):
         if self.isLeaf(pos):
             return
         
-        if self.Heap[pos] > self.Heap[self.getLeft(pos)] or self.Heap[pos] > self.Heap[self.getRight(pos)]:
-            if self.Heap[self.getLeft(pos)] < self.Heap[self.getRight(pos)]:
-                self.swap(pos,self.getLeft(pos))
-                self.minHeapify(self.getLeft(pos))
-            else:
-                self.swap(pos,self.getRight(pos))
-                self.minHeapify(self.getRight(pos))
-    
-    def minHeap(self):
+        left = self.getLeft(pos)
+        right = self.getRight(pos)
 
-        for pos in range(self.size // 2 , 0, -1):
+        # Check if children are within bounds and find the smallest child
+        if left <= self.size and self.Heap[pos] > self.Heap[left]:
+            smallest = left
+        else:
+            smallest = pos
+
+        if right <= self.size and self.Heap[smallest] > self.Heap[right]:
+            smallest = right
+
+        # If the current node is not the smallest, swap and heapify
+        if smallest != pos:
+            self.swap(pos, smallest)
+            self.minHeapify(smallest)
+
+    def minHeap(self):
+        for pos in range(self.size // 2, 0, -1):
             self.minHeapify(pos)
 
-    def insert(self,element):
-
+    def insert(self, element):
         if self.size >= self.max_size:
             return
 
-        self.size+=1
+        self.size += 1
         self.Heap[self.size] = element
         current = self.size
 
-        while self.Heap[self.getParent(current)] > self.Heap[current]:
-            self.swap(current,self.getParent(current))
+        # Move the newly inserted element to its correct position
+        while current > 1 and self.Heap[self.getParent(current)] > self.Heap[current]:
+            self.swap(current, self.getParent(current))
             current = self.getParent(current)
 
     def remove(self):
-        popped = self.Heap[1]
-        self.Heap[1] = self.Heap[self.size-1]
-        self.Heap[self.size-1] = popped
-        self.minHeapify(1)
+        if self.size == 0:
+            return None
+
+        popped = self.Heap[1]  # Min element is at the root (1st index)
+        self.Heap[1] = self.Heap[self.size]  # Move last element to root
         self.size -= 1
+        self.minHeapify(1)
         return popped
 
+    def print_heap(self):
+        # Print the heap level by level
+        for i in range(1, (self.size // 2) + 1):
+            left = self.getLeft(i)
+            right = self.getRight(i)
 
-    def print(self):
-        for i in range(1,(self.size//2) + 1):
-            print("Parent : ", self.Heap[i])
-            print("Parent : ", self.Heap[self.getLeft(i)])
-            print("Parent : ", self.Heap[self.getRight(i)])
+            print(f"Parent: {self.Heap[i]}")
+            if left <= self.size:
+                print(f" Left Child: {self.Heap[left]}")
+            if right <= self.size:
+                print(f" Right Child: {self.Heap[right]}")
+            print()
 
-print('The minHeap is ')
+# Example usage
+print('The minHeap is:')
 minHeap = MinHeap(15)
 minHeap.insert(5)
 minHeap.insert(3)
@@ -78,8 +94,8 @@ minHeap.insert(22)
 minHeap.insert(9)
 minHeap.minHeap()
 
-#minHeap.print()
+minHeap.print_heap()
 
-minHeap.remove()
+print("Removed element:", minHeap.remove())
 
-minHeap.print()
+minHeap.print_heap()
